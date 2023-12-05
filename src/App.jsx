914990eddfe2d771243469e8c64dfa6ba8d6898b
vikash-frontier <vikash@frontier.xyz>
2023-12-05 19:46:06 +0530

    added font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { categories } from "./utils/mockData";
-import "./categories.styles.scss";
 import Directory from "./components/directory/Directory";
 function App() {
   return <Directory categories={categories} />;
