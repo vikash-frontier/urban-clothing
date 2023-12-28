b092b4b86219b027c5a39880d12cd962cbbe22bb
vikash-frontier <vikash@frontier.xyz>
2023-12-29 00:16:59 +0530

    Added the style for cart page

--- src/routes/checkout/Checkout.jsx
+++ src/routes/checkout/Checkout.jsx
@@ -8,9 +8,28 @@ const Checkout = () => {
   const { cartItems } = useContext(CartContext);
   return (
     <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Descreption</span>
+        </div>
+        <div className="header-block">
+          <span>Qunatity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
       {cartItems.map((item) => (
         <CheckoutItem key={item.id} cartItem={item} />
       ))}
+
+      <span className="total">Total: 0</span>
     </div>
   );
 };
