8d3ddcda21ff091ce89962810474f815b5dc9893
vikash-frontier <vikash@frontier.xyz>
2023-12-31 01:09:48 +0530

    Showing all the categories in shop

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import { ProductsContext } from "../../context/ProductContext";
 import ProductCard from "../../components/product-card/ProductCard";
 import "./shop.styles.scss";
@@ -7,11 +7,18 @@ const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <div className="products-contaier">
-      {/* {products.map((product) => (
-        <ProductCard key={product.id} products={product} />
-      ))} */}
-    </div>
+    <Fragment>
+      {Object.keys(products).map((title) => (
+        <Fragment key={title}>
+          <h1>{title}</h1>
+          <div className="products-contaier">
+            {products[title].map((product) => (
+              <ProductCard key={product.id} products={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
