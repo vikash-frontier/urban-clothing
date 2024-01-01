c90a224dc2191f3662dab8606431a8e4420057f2
vikash-frontier <vikash@frontier.xyz>
2024-01-01 19:16:25 +0530

    Added the empty cart icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./crown-logo.svg";
 import shoppingIcon from "./shopping-bag.svg";
+import epmtyCart from "./icons8-empty-cart.gif";
 
-export { logo, shoppingIcon };
+export { logo, shoppingIcon, epmtyCart };
--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -6,6 +6,7 @@ import Button from "../button/Button";
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/CartItem";
 import { CartContext } from "../../context/CartContex";
+import { epmtyCart } from "../../assets";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -18,11 +19,19 @@ const CartDropdown = () => {
 
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems?.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+      {cartItems.length === 0 ? (
+        <div className="empty-cart">
+          <img src={epmtyCart} alt="empty-cart" />
+          <p>Your cart is empty</p>
+        </div>
+      ) : (
+        <div className="cart-items">
+          {cartItems?.map((item) => (
+            <CartItem key={item.id} cartItem={item} />
+          ))}
+        </div>
+      )}
+
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
